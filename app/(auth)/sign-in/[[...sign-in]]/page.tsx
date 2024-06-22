92c99bdfa91d727c5550f492331b3abeedcc0f2f
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-06-22 12:42:54 +0530

    authUpdate

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn, SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <div>
+            <SignIn/>
+        </div>
+    </section>
+  )
+}
+
+export default page
