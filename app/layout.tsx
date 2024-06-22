f1c7dee0146d444a730067639fcaf41fde583608
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-06-22 13:17:51 +0530

    LayOutTesting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +22,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={cn("min-h-screen h-dvh bg-background font-sans antialiased", fontSans.variable)}>
+          <div className="">
+
+          </div>
+          {children}
+          
+          </body>
       </html>
     </ClerkProvider>
   );
